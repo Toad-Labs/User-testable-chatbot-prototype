7a05cf88151ecc2666e64b0958d90ce70187a598
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-04 08:07:53 -0500

    Remove unused code

--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -17,39 +17,6 @@ export default {
   },
   setup() {
 
-/*
-    const textToSend = ref([]);
-
-    const messages = ref([]);
-
-
-    var ably = new Ably.Realtime('BiSQUw.FNx0Ig:b_cUpgQ-rBUTDFBz');
-    var channel = ably.channels.get('emerald-helper');
-
-    channel.subscribe('message', function(message) {
-      console.log("Message", message);
-      messages.value.push({text: message.data.text });
-    });
-
-    function sendMessage () {
-
-      // To Do: check if message is blank or something later
-      // Publish a message to the gut-cam channel
-      if (textToSend.value.length > 0) {
-        channel.publish('message', { text: textToSend.value, user: 'other guy'});
-        console.log('Controller: Send Message send Event');
-      }
-
-    }
-
-    return {
-      textToSend,
-      messages,
-      sendMessage
-    }
-
-    */
-
   }
 }
 </script>
